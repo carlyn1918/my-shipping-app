0689e3a735bf44fa231ae7faa8b7341e1ed692ad
CBL <133843215+carlyn1918@users.noreply.github.com>
2024-10-29 19:24:52 +0800

    re arange how the info output displayed

--- src/components/ConfirmationForm.jsx
+++ src/components/ConfirmationForm.jsx
@@ -1,6 +1,6 @@
 // ConfirmationForm.jsx
-import React from 'react';
-import { Typography, Button } from '@mui/material';
+import React from "react";
+import { Typography, Button } from "@mui/material";
 
 const ConfirmationForm = ({ data, onBack, onConfirm }) => {
   return (
@@ -8,18 +8,21 @@ const ConfirmationForm = ({ data, onBack, onConfirm }) => {
       <Typography variant="h6">Confirm Your Details</Typography>
 
       <Typography variant="subtitle1">Sender Information</Typography>
-      <div>Sender: {data.sender.address}, {data.sender.region}, {data.sender.city}</div>
-      
+      <div>
+        Sender: {data.sender.address}, {data.sender.region}, {data.sender.city}
+      </div>
 
-      <Typography variant="subtitle1">Receiver Information</Typography>
-      <div>Receiver: {data.receiver.address}, {data.receiver.region}, {data.receiver.city}</div>
+ 
+
 
       {data.stopovers.length > 0 && (
         <>
           <Typography variant="subtitle1">Stopover Information</Typography>
           {data.stopovers.map((stop, index) => (
             <div key={index}>
-              <div>Stopover {index + 1} Address: {stop.address}</div>
+              <div>
+                Stopover {index + 1} Address: {stop.address}
+              </div>
               <div>City: {stop.region}</div>
               <div>Province: {stop.city}</div>
             </div>
@@ -27,13 +30,18 @@ const ConfirmationForm = ({ data, onBack, onConfirm }) => {
         </>
       )}
 
+<Typography variant="subtitle1">Receiver Information</Typography>
+<div>Receiver: {data.receiver.address}, {data.receiver.region}, {data.receiver.city}</div>
+
       <Typography variant="subtitle1">Parcel Information</Typography>
       <div>Weight: {data.parcel.weight} KG</div>
       <div>Vehicle Type: {data.parcel.vehicleType}</div>
 
       <Typography variant="subtitle1">Payment Details</Typography>
       <div>Method: {data.payment.method}</div>
-      {data.payment.method === 'COD' && <div>COD Amount: PHP {data.payment.codAmount}</div>}
+      {data.payment.method === "COD" && (
+        <div>COD Amount: PHP {data.payment.codAmount}</div>
+      )}
 
       <Button onClick={onBack}>Back</Button>
       <Button onClick={onConfirm} variant="contained" color="primary">
